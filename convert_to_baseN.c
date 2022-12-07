7e0e57e962f0ade49f5b5c81b3dfbb67c0588b95
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-12-07 15:53:19 +0000

    fixed type overflow

--- convert_to_baseN.c
+++ convert_to_baseN.c
@@ -1,24 +1,26 @@
 #include "main.h"
-
 /**
  * convert_to_base - converts a signed interger to base b
- * @num: signed integer
+ * @_num: signed integer
  * @base: the new base
  * @lowercase: 1 = lowercase or 0 =  uppercase
  *
  * Return: number of characters written to STDOUT
  */
-size_t convert_to_base(long int num, int base, size_t lowercase)
+size_t convert_to_base(long int _num, int base, size_t lowercase)
 {
 	char *chars;
 	size_t nwrite;
+	unsigned long int num;
 
 	nwrite = 0;
-	if (num < 0 && base == 10)
+	if (_num < 0 && base == 10)
 	{
-		num *= -1;
+		num = -_num;
 		nwrite += _putchar('-');
 	}
+	else
+		num = _num;
 
 	chars = (lowercase)
 		? "0123456789abcdef"
